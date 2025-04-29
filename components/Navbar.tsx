845e07d882bbe613c8ca94bfeceba027545a2b1b
lipon <liponbdbd@gmail.com>
2025-04-29 17:21:39 +0600

    created simple navbar and footer components

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+
+const Navbar = () => {
+
+  return (
+    <nav className="bg-white shadow px-4 py-3">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold text-orange-600">
+        BD-Foody-Zone
+        </Link>
+
+        <div className="space-x-4 flex items-center">
+          <Link href="/" className="text-gray-700 hover:text-orange-500">
+            Home
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
