d68e4023b45e75cf5ac4d8313da672096d3d9dd0
lipon <liponbdbd@gmail.com>
2025-07-17 11:03:05 +0600

    created increment and decrement food cart slice

--- app/store/useCartStore.tsx
+++ app/store/useCartStore.tsx
@@ -1,55 +1,93 @@
-
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { IFood } from "@/Type";
 
-
 interface ICartStore {
-    cartProducts: IFood[];
-    totalQuantity: number;
-    totalPrice: number;
-    addItemToCart: (newItem: IFood) => void;
+  cartProducts: IFood[];
+  totalQuantity: number;
+  totalPrice: number;
+  addItemToCart: (newItem: IFood) => void;
+  incrementCart: (newItem: IFood) => void;
+  decrementCart: (newItem: IFood) => void;
 }
 
 const useCartStore = create<ICartStore>()(
-    devtools(persist(
-        (set) => ({
-            cartProducts: [],
-            totalQuantity: 0,
-            totalPrice: 0,
-            addItemToCart: (newItem) => {
-                set((state) => {
-                    const itemIndex = state.cartProducts.findIndex(
-                        (item) => item.id === newItem.id
-                    );
+  devtools(
+    persist(
+      (set) => ({
+        cartProducts: [],
+        totalQuantity: 0,
+        totalPrice: 0,
+        addItemToCart: (newItem) => {
+          set((state) => {
+            const itemIndex = state.cartProducts.findIndex(
+              (item) => item.id === newItem.id
+            );
 
-                    if (itemIndex >= 0) {
-                        const updatedCart = [...state.cartProducts];
-                        const updatedItem = { ...updatedCart[itemIndex] };
-                        updatedItem.cartQuantity += 1;
-                        updatedCart[itemIndex] = updatedItem;
-                        return {
-                            ...state,
-                            cartProducts: updatedCart,
-                        };
-                    } else {
-                    
-                        return {
-                            ...state,
-                            cartProducts: [
-                                ...state.cartProducts,
-                                { ...newItem, cartQuantity: 1 },
-                            ],
+            if (itemIndex >= 0) {
+              const updatedCart = [...state.cartProducts];
+              const updatedItem = { ...updatedCart[itemIndex] };
+              updatedItem.cartQuantity += 1;
+              updatedCart[itemIndex] = updatedItem;
+              return {
+                ...state,
+                cartProducts: updatedCart,
+              };
+            } else {
+              return {
+                ...state,
+                cartProducts: [
+                  ...state.cartProducts,
+                  { ...newItem, cartQuantity: 1 },
+                ],
+              };
+            }
+          });
+        },
+        incrementCart: (newItem) => {
+          set((state) => {
+            const itemIndex = state.cartProducts.findIndex(
+              (item) => item.id === newItem.id
+            );
 
+            const updatedCart = [...state.cartProducts];
+            const updatedItem = { ...updatedCart[itemIndex] };
+            if (updatedItem.cartQuantity >= 1) {
+              updatedItem.cartQuantity += 1;
+            }
+            updatedCart[itemIndex] = updatedItem;
 
-                        };
-                    }
-                });
-            },
+            return {
+              ...state,
+              cartProducts: updatedCart,
+            };
+          });
+        },
+        decrementCart: (newItem) => {
+          set((state) => {
+            const itemIndex = state.cartProducts.findIndex(
+              (item) => item.id === newItem.id
+            );
 
-        }),
-        { name: "CartStore" }
-    ), { name: 'CartStore', getStorage: () => localStorage })
+            const updatedCart = [...state.cartProducts];
+            const updatedItem = { ...updatedCart[itemIndex] };
+
+            if (updatedItem.cartQuantity > 1) {
+              updatedItem.cartQuantity -= 1;
+            }
+            updatedCart[itemIndex] = updatedItem;
+
+            return {
+              ...state,
+              cartProducts: updatedCart,
+            };
+          });
+        },
+      }),
+      { name: "CartStore" }
+    ),
+    { name: "CartStore", getStorage: () => localStorage }
+  )
 );
 
 export default useCartStore;
